da0dd231740cc92f43980b224db7df58ef2a3d36
Mabon Enundu <ndschalk@gmail.com>
2019-11-09 17:43:08 -0600

    Groups Scaffold

--- flight_plan/config/routes.rb
+++ flight_plan/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :groups
   get "clients" => 'clients#index'
   get "schedule" => "schedule#index"
   get "users" => "users#index"
--- flight_plan/db/schema.rb
+++ flight_plan/db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191109230854) do
+ActiveRecord::Schema.define(version: 20191109233958) do
 
   create_table "clients", force: :cascade do |t|
     t.string "client_name"
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "nickname"
+    t.string "level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
